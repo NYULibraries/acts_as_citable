3dcb802808ee3ae6770dd17a8ef8b7ab9cd83353
hab278 <as>
2013-04-03 15:42:58 -0400

    Fixed test

--- test/acts_as_citable_test.rb
+++ test/acts_as_citable_test.rb
@@ -47,7 +47,7 @@ class RecordTest < ActiveSupport::TestCase
     rec = Record.create(:data => "itemType: book", :format => "csf")
     assert rec.to_ris.eql? "TY  - BOOK\nER  -\n\n"
     assert rec.to_bibtex.eql? "@book{????\n}"
-    assert rec.to_openurl.eql? "?rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book"
+    assert rec.to_openurl.eql? "rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book"
     assert rec.to_easybib.eql? "{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[{}]}"
   end
   
