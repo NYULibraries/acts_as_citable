6e80796a60e60aa3678eebe51b5081c0f47c11e6
hab278 <hab278@nyu.edu>
2014-12-17 13:55:01 -0500

    Move unit tests to where they belong

--- test/dummy/test/unit/inherited_record_test.rb
+++ test/dummy/test/unit/inherited_record_test.rb
@@ -1,25 +1,25 @@
 require 'test_helper'
 
-class RecordTest < ActiveSupport::TestCase
-  test "should be able to create a new Record" do
-    rec = Record.new(:data => "itemType: book", :format => "csf")
+class InheritedRecordTest < ActiveSupport::TestCase
+  test "should be able to create a new Inherited Record" do
+    rec = InheritedRecord.new(:data => "itemType: book", :format => "csf")
     assert_equal "TY  - BOOK\nER  -\n\n", rec.to_ris
     assert_equal "@book{????\n}", rec.to_bibtex
     assert_equal "rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book", rec.to_openurl
     assert_equal "{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[]}", rec.to_easybib
   end
 
-  test "should check if i can use the record" do
-    assert rec = Record.new( :data => "data", :format => "from_format")
+  test "should check if i can use the Inherited record" do
+    assert rec = InheritedRecord.new( :data => "data", :format => "from_format")
     assert rec.destroy
   end
 
   test "should check to see if format fields are set" do
-    assert 'format', Record.format_field
+    assert 'format', InheritedRecord.format_field
   end
 
-  test "should check to see if records can be converted" do
-    rec = Record.new(:data => "itemType: book", :format => "csf")
+  test "should check to see if Inherited records can be converted" do
+    rec = InheritedRecord.new(:data => "itemType: book", :format => "csf")
     assert rec.respond_to?(:to_ris)
     assert rec.respond_to?(:to_bibtex)
     assert rec.respond_to?(:to_openurl)
@@ -30,8 +30,17 @@ class RecordTest < ActiveSupport::TestCase
     assert rec.destroy
   end
 
+  # test "should work with models inherited acts_as_citable models" do
+  #   rec = InheritedRecord.new(:data => "itemType: book", :format => "csf")
+  #   assert_equal "TY  - BOOK\nER  -\n\n", rec.to_ris
+  #   assert_equal "@book{????\n}", rec.to_bibtex
+  #   assert_equal "rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book", rec.to_openurl
+  #   assert_equal "{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[]}", rec.to_easybib
+  #   assert rec.destroy
+  # end
+
   test "should check to see if you cannot convert from" do
-    rec = Record.new(:data => "itemType: book", :format => "csf")
+    rec = InheritedRecord.new(:data => "itemType: book", :format => "csf")
     assert !rec.respond_to?(:from_ris)
     assert !rec.respond_to?(:from_bibtex)
     assert !rec.respond_to?(:from_openurl)
@@ -48,7 +57,7 @@ class RecordTest < ActiveSupport::TestCase
   end
 
   test "should be available to convert to" do
-    rec = Record.new(:data => "itemType: book", :format => "csf")
+    rec = InheritedRecord.new(:data => "itemType: book", :format => "csf")
     assert_equal "TY  - BOOK\nER  -\n\n", rec.to_ris
     assert_equal "@book{????\n}", rec.to_bibtex
     assert_equal "rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book", rec.to_openurl
@@ -58,14 +67,14 @@ class RecordTest < ActiveSupport::TestCase
 
   test "arrays should be able to convert to ris and bibtex" do
     arr = Array.new
-    arr << Record.new(:data => "itemType: book", :format => "csf") << Record.create(:data => "itemType: book", :format => "csf")
+    arr << InheritedRecord.new(:data => "itemType: book", :format => "csf") << InheritedRecord.create(:data => "itemType: book", :format => "csf")
     assert arr.to_bibtex
     assert arr.to_ris
     arr.each {|rec| assert rec.destroy}
   end
 
   test "should be able to convert to an export STYLE" do
-    rec = Record.new(:data => "itemType: book", :format => "csf")
+    rec = InheritedRecord.new(:data => "itemType: book", :format => "csf")
     assert_equal "<div class=\"csl-bib-body\">\n  <div class=\"csl-entry\"> (n.d.).</div>\n</div>", rec.to_apa
     assert_equal "<div class=\"csl-bib-body\">\n  <div class=\"csl-entry\"> Print.</div>\n</div>", rec.to_mla
     assert_equal "<div class=\"csl-bib-body\">\n\n[CSL STYLE ERROR: reference with no printed form.]\n</div>", rec.to_chicago_author_date
--- test/dummy/test/unit/record_changed_field_test.rb
+++ test/dummy/test/unit/record_changed_field_test.rb
@@ -1,7 +1,73 @@
 require 'test_helper'
 
 class RecordChangedFieldTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be able to create a new Record with changed fields" do
+    rec = RecordChangedField.new(:data => "itemType: book", :from_format => "csf")
+    assert_equal "TY  - BOOK\nER  -\n\n", rec.to_ris
+    assert_equal "@book{????\n}", rec.to_bibtex
+    assert_equal "rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book", rec.to_openurl
+    assert_equal "{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[]}", rec.to_easybib
+  end
+
+  test "should check if i can use the record with changed fields" do
+    assert rec = RecordChangedField.new( :data => "data", :from_format => "from_format")
+    assert rec.destroy
+  end
+
+  test "should check to see if from_format fields are set" do
+    assert 'from_format', RecordChangedField.format_field
+  end
+
+  test "should check to see if records with changed fields can be converted" do
+    rec = RecordChangedField.new(:data => "itemType: book", :from_format => "csf")
+    assert rec.respond_to?(:to_ris)
+    assert rec.respond_to?(:to_bibtex)
+    assert rec.respond_to?(:to_openurl)
+    assert rec.respond_to?(:to_csf)
+    assert rec.respond_to?(:to_easybib)
+    assert rec.respond_to?(:to_refworks_tagged)
+    assert rec.respond_to?(:csf)
+    assert rec.destroy
+  end
+
+  test "should check to see if you cannot convert from" do
+    rec = RecordChangedField.new(:data => "itemType: book", :from_format => "csf")
+    assert !rec.respond_to?(:from_ris)
+    assert !rec.respond_to?(:from_bibtex)
+    assert !rec.respond_to?(:from_openurl)
+    assert !rec.respond_to?(:from_csf)
+    assert !rec.respond_to?(:from_pnx)
+  end
+
+  test "should raise no method error" do
+    rec = RecordChangedField.new(:data => "itemType: book", :from_format => "csf")
+    assert_raise NoMethodError do
+      rec.from_ris
+    end
+    assert rec.destroy
+  end
+
+  test "should be available to convert to" do
+    rec = RecordChangedField.new(:data => "itemType: book", :from_format => "csf")
+    assert_equal "TY  - BOOK\nER  -\n\n", rec.to_ris
+    assert_equal "@book{????\n}", rec.to_bibtex
+    assert_equal "rft.ulr_ver=Z39.88-2004&rft.ctx_ver=Z39.88-2004&rft.rfr_id=info:sid/libraries.nyu.edu:citero&rft_val_fmlt=info:ofi/fmt:kev:mtx:book&rft.genre=book", rec.to_openurl
+    assert_equal "{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[]}", rec.to_easybib
+    assert rec.destroy
+  end
+
+  test "arrays should be able to convert to ris and bibtex" do
+    arr = Array.new
+    arr << RecordChangedField.new(:data => "itemType: book", :from_format => "csf") << RecordChangedField.create(:data => "itemType: book", :from_format => "csf")
+    assert arr.to_bibtex
+    assert arr.to_ris
+    arr.each {|rec| assert rec.destroy}
+  end
+
+  test "should be able to convert to an export STYLE" do
+    rec = RecordChangedField.new(:data => "itemType: book", :from_format => "csf")
+    assert_equal "<div class=\"csl-bib-body\">\n  <div class=\"csl-entry\"> (n.d.).</div>\n</div>", rec.to_apa
+    assert_equal "<div class=\"csl-bib-body\">\n  <div class=\"csl-entry\"> Print.</div>\n</div>", rec.to_mla
+    assert_equal "<div class=\"csl-bib-body\">\n\n[CSL STYLE ERROR: reference with no printed form.]\n</div>", rec.to_chicago_author_date
+  end
 end
